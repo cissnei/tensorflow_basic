8f6596250685e0835484fbb4029a64dce016fa9b
cissnei <cissnei@naver.com>
2017-05-01 18:29:03 +0900

    first

--- ComputationalGraph.py
+++ ComputationalGraph.py
@@ -11,3 +11,14 @@ print("node1: ", node1) # it prints just structure of tensor
 sess=tf.Session()
 print(sess.run([node1, node2]))
 print(sess.run(node3))
+
+#Placeholder
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b # same as 'add(a,b)'
+
+print (sess.run(adder_node, feed_dict ={a:3.0, b: 4.5}))
+print (sess.run(adder_node, feed_dict={a:[1,3], b:[2,5]}))
+
+
