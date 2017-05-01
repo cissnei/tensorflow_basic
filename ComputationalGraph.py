3942e98090f2ad8bfc8c4c49546531a4907d0206
cissnei <cissnei@naver.com>
2017-05-01 18:20:26 +0900

    first

--- ComputationalGraph.py
+++ ComputationalGraph.py
@@ -5,3 +5,9 @@ import tensorflow as tf
 node1 = tf.constant(3.0, tf.float32)
 node2 = tf.constant(4.0, tf.float32)
 node3 = tf.add(node1, node2)
+
+print("node1: ", node1) # it prints just structure of tensor
+
+sess=tf.Session()
+print(sess.run([node1, node2]))
+print(sess.run(node3))
